204b47efd8884bf57fa800af095ea25537a354be
张文军 <zhangwenjun@xiaobu121.com>
2020-04-24 14:20:10 +0800

    Initial commit

---
+++ Example/FFSpecialKit_Category/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFSpecialKit_Category
+//
+//  Created by zhangwenjun0630@163.com on 04/24/2020.
+//  Copyright (c) 2020 zhangwenjun0630@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
